2248898df6c5dd5e888c8c92411883a98a52b2bf
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-09-28 15:00:31 +0530

    error

--- generate_certificates/src/App.jsx
+++ generate_certificates/src/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
-import Home from './Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { store } from './app/store';
+import { checkAuth } from './features/auth/authSlice';
+import Register from '/pages/register';
+import Login from '/pages/Login';
+import HomePage from './pages/HomePage';
+import { Toaster } from "@/components/ui/toaster";
+import NotFound from './pages/NotFound';
+// import { RootState } from './app/store';
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      {/* <Home /> */}
-      {/* <Register /> */}
-      <Login />
-    </>
-  )
+function PrivateRoute({ children }) {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />;
 }
 
-export default App
- 
+function AppContent() {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+        <Route 
+          path="*" 
+         element= {<NotFound/>} 
+        />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+}
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  );
+}
+
+export default App;
